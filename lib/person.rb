6b19d01ba257a498a9670a1e3fefa49b5965e68b
jonathanboss17 <jtboss47@gmail.com>
2019-07-17 15:40:32 -0400

    completely done this time

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,8 @@
 require 'pry'
 
  class Person 
-    attr_accessor :name, :bank_account, :happiness, :hygiene
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
 
     def initialize(name)
         @name = name
@@ -11,12 +12,33 @@ require 'pry'
         @hygiene = 8
     end
 
+    def hygiene=(new_hygiene)
+        if(new_hygiene < 0)
+            @hygiene = 0
+        elsif(new_hygiene > 10)
+            @hygiene = 10 
+        else 
+            @hygiene = new_hygiene
+        end
+    end
+
+
+    def happiness=(new_happiness)
+        if(new_happiness < 0)
+            @happiness = 0
+        elsif(new_happiness > 10)
+            @happiness = 10 
+        else 
+            @happiness = new_happiness
+        end
+    end
+
     def clean? 
-        @hygiene >=0 && @hygiene <=10 && @hygiene > 7
+        @hygiene > 7
     end
     
     def happy?
-        @happiness >=0 && @happiness <=10 && @happiness > 7
+        @happiness > 7
     end
 
     def get_paid(salary_amount)
@@ -25,49 +47,30 @@ require 'pry'
     end
 
     def take_bath
-       if @hygiene < 7 
-            @hygiene += 4
-        else 
-            @hygiene = 10
-        end
+        self.hygiene += 4
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out 
-        if @happiness < 9 
-            @happiness += 2 
-        else 
-            @happiness = 10 
-        end
-
-        if @hygiene < 3 
-            @hygiene = 0 
-        else 
-            @hygiene -= 3
-        end
+        self.hygiene -= 3
+        self.happiness +=2
         "♪ another one bites the dust ♫"
     end
     
     def call_friend(friend)
-        if(self.happiness < 8 && friend.happiness < 8)
             self.happiness += 3
             friend.happiness += 3
-        else 
-            self.happiness = 10
-            friend.happiness = 10
-        end
-
         "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
 
     def start_conversation(person, topic)
         if topic == "politics"
-            self.happiness -= 2
+            self.happiness -=2
             person.happiness -= 2
             return "blah blah partisan blah lobbyist"
         elsif topic == "weather"
-            self.happiness += 2
-            person.happiness += 2
+            self.happiness += 1
+            person.happiness += 1
             return "blah blah sun blah rain"
         else 
             return "blah blah blah blah blah"
@@ -75,8 +78,6 @@ require 'pry'
     end
  end
 
-person = Person.new("Courtney")
-friend = Person.new("Jon")
-binding.pry
+
 
  
